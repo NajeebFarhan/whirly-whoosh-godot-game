6f81ce690d3f4994c5777086e9157f40091167d5
Najeeb Farhan <najeebfarhan6345@gmail.com>
2024-09-16 19:06:47 +0530

    first setup

---
+++ scripts/charactermovement.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var angular_velocity: float = 3 
+
+func _ready():
+	print("script initiated")
+
+func _physics_process(delta):
+	rotate(angular_velocity * delta)
+	
+	if Input.is_action_just_pressed("toggle_rotation"):
+		angular_velocity *= -1
