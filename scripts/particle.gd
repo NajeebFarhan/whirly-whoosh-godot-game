09b8b64bbf5dba86f3bec0461e78fc4475c822a5
Najeeb Farhan <najeebfarhan6345@gmail.com>
2024-10-07 11:17:30 +0530

    I ditched building the help-page

--- scripts/particle.gd
+++ scripts/particle.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var particle = $CPUParticles2D
+@onready var sprite = $Sprite2D
 var particle_velocity := Vector2(-8, 0)
 	
 func _physics_process(_delta):
@@ -14,8 +16,16 @@ func _on_area_entered(_area):
 	
 func _on_body_entered_obstacle(_body):
 	GameManager.decrease_health()
-	call_deferred("queue_free")
+	sprite.visible = false
+	particle.emitting = true
 
 func _on_body_entered_collectable(_body):
 	GameManager.increase_score()
+	sprite.visible = false
+	particle.emitting = true
+
+func _on_cpu_obstacle_finished():
+	call_deferred("queue_free")
+
+func _on_cpu_collectable_finished():
 	call_deferred("queue_free")
